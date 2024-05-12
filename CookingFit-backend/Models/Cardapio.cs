8f719e68f92224d4d9ce34eaa4b640a0f3522d5b
Emilia-ads <144388125+Emilia-ads@users.noreply.github.com>
2024-05-12 11:21:08 -0300

    Configuração dos Ingredientes e Cardápio, e Banco de dados.

---
+++ CookingFit-backend/Models/Cardapio.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CookingFit_backend.Models
+{
+    [Table("Cardápio")]
+    public class Cardapio
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório, informar a descricao!")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório, informar o total de calorias!")]
+        public int Calorias { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório, informar a quantidade! (Ex.: 1 colher, meia chícara, etc.)")]
+        public string Quantidade { get; set; }
+
+    }
+}
